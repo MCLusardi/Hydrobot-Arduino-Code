bfce549fc5f3e245d1c98058fc2215f2ef0b4969
MC Lusardi <mclntg@mail.missouri.edu>
2020-11-05 12:35:21 -0600

    Fixed number for serial read in

--- arduino_mega_DO_sample_code/arduino_mega_DO_sample_code.ino
+++ arduino_mega_DO_sample_code/arduino_mega_DO_sample_code.ino
@@ -16,7 +16,7 @@ float DO;                                             //used to hold a floating 
 
 void setup() {                                        //set up the hardware
   Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
-  Serial1.begin(9600);                                //set baud rate for software serial port_3 to 9600
+  Serial3.begin(9600);                                //set baud rate for software serial port_3 to 9600
   inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
   sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
 }
@@ -24,11 +24,12 @@ void setup() {                                        //set up the hardware
 void serialEvent() {                                  //if the hardware serial port_0 receives a char
   inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
   input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
+  //Serial.println("Input received");
 }
 
 
-void serialEvent1() {                                 //if the hardware serial port_3 receives a char
-  sensorstring = Serial1.readStringUntil(13);         //read the string until we see a <CR>
+void serialEvent3() {                                 //if the hardware serial port_3 receives a char
+  sensorstring = Serial3.readStringUntil(13);         //read the string until we see a <CR>
   sensor_string_complete = true;                      //set the flag used to tell if we have received a completed string from the PC
 }
 
@@ -37,10 +38,11 @@ void loop() {                                         //here we go...
 
 
   if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
-    Serial1.print(inputstring);                       //send that string to the Atlas Scientific product
-    Serial1.print('\r');                              //add a <CR> to the end of the string
+    Serial3.print(inputstring);                       //send that string to the Atlas Scientific product
+    Serial3.print('\r');                              //add a <CR> to the end of the string
     inputstring = "";                                 //clear the string
     input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+    //Serial.println("Input sent to DO");
   }
 
 
